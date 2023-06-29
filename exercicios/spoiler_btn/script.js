084b88a6e504026f8b4c7a39b538f27371f56476
Felipe Souza <fssouz@hotmail.com>
2023-06-29 16:05:38 -0300

    button spoiler
    new exercise

---
+++ exercicios/spoiler_btn/script.js
@@ -0,0 +1,10 @@
+var a=0
+function show(){
+    var d = document.getElementById('content')
+    if(d.style.display == 'block'){
+        d.style.display = 'none';
+    }
+    else{
+        d.style.display = 'block'
+    }
+}
